ac38f6f96195310b00e98848d654c176f4c78a00
MunicWi <65843440+MunicWi@users.noreply.github.com>
2020-05-25 23:31:07 +0700

    Add files via upload

--- Group6.cs
+++ Group6.cs
@@ -20,43 +20,43 @@ namespace Taxx
         private void button1_Click(object sender, EventArgs e)
         {
             int outcomefinal = int.Parse(textBox1.Text);
-            int k;
+            int pay;
 
 
             if (outcomefinal > 5000001)
             {
-                k = (outcomefinal * 35 / 100);
+                pay = (outcomefinal * 35 / 100);
             }
             else if (outcomefinal > 2000001)
             {
-                k = (outcomefinal * 30 / 100);
+                pay = (outcomefinal * 30 / 100);
             }
             else if (outcomefinal > 1000001)
             {
-                k = (outcomefinal * 25 / 100);
+                pay = (outcomefinal * 25 / 100);
             }
             else if (outcomefinal > 750001)
             {
-                k = (outcomefinal * 20 / 100);
+                pay = (outcomefinal * 20 / 100);
             }
             else if (outcomefinal > 500001)
             {
-                k = (outcomefinal * 15 / 100);
+                pay = (outcomefinal * 15 / 100);
             }
             else if (outcomefinal > 300001)
             {
-                k = (outcomefinal * 10 / 100);
+                pay = (outcomefinal * 10 / 100);
             }
             else if (outcomefinal > 150001)
             {
-                k = (outcomefinal * 5 / 100);
+                pay = (outcomefinal * 5 / 100);
             }
             else
             {
-                k = (0);
+                pay = (0);
             }
 
-            textBox2.Text = k.ToString();
+            textBox2.Text = pay.ToString();
         }
 
         private void button2_Click(object sender, EventArgs e)
